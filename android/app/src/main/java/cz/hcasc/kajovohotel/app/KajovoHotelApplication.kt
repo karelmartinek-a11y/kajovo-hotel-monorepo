8c4844b8e8bdd5ab8626d0268c5c895d742f9c54
karelmartinek-a11y <karel.martinek@post.cz>
2026-05-05 08:40:27 +0200

    chore: android runtime marker pro import snidani parity

--- android/app/src/main/java/cz/hcasc/kajovohotel/app/KajovoHotelApplication.kt
+++ android/app/src/main/java/cz/hcasc/kajovohotel/app/KajovoHotelApplication.kt
@@ -4,4 +4,8 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class KajovoHotelApplication : Application()
+class KajovoHotelApplication : Application() {
+    companion object {
+        const val BREAKFAST_MULTIDAY_IMPORT_ENABLED: Boolean = true
+    }
+}
